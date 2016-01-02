031f7a60ba77f7983852ed4d71da3fa7ad4b2c95
hirikur <hirikur+git@gmail.com>
2016-01-03 01:25:25 +0800

    滚动效果改为平滑，修复了一个小bug。

--- 3.carousel form Baidu IFE/scripts/carousel.js
+++ 3.carousel form Baidu IFE/scripts/carousel.js
@@ -1,42 +1,81 @@
-window.onload=function() {
-var oImgBox=document.getElementsByTagName("div")[0];
-var oImgList=oImgBox.getElementsByTagName("ul")[0];
-var aImg=oImgList.getElementsByTagName("img");
-var aBtnList=oImgBox.getElementsByTagName("ul")[1];
-var aBtn=aBtnList.getElementsByTagName("li");
-var timer=null;
-var i = 0;
-var index = 0;
-var isBorder = true;
+function carousel(){
+	var oImgBox = document.getElementsByTagName("div")[0];
+	var oImgList = oImgBox.getElementsByTagName("ul")[0];
+	var aImg = oImgList.getElementsByTagName("img");
+	var aBtnList = oImgBox.getElementsByTagName("ul")[1];
+	var aBtn = aBtnList.getElementsByTagName("li");
+	var timer = null;
+	var autoTimer = null;
+	var i = 0;
+	var index = 0;
+	var isBorder = true;
 
-/*function doMove(node,offset){
-	var speed = offfset/10;
-	var oldOffsetLeft = node.offsetLeft;
-	speed = spped > 0 ? Math.ceil(speed) : Math.floor(speed);
-	node.offsetLeft - oldOffsetLsft==offset ? clearInterval(timer):node.style.left=node.offsetLeft+speed+"px";
-}*/
-
-function doChange(node,off){
-	node.style.left=(-off*aImg[0].offsetWidth)+"px";
-}
-
-function next(){
-	isBorder ? index++ : index--;
-	if(index<=0)
-		isBorder=true;
-	if(index>=aImg.length-1)
-		isBorder=false;
-	cutover();
-}
-
-function cutover(){
-	for(i=0;i<aImg.length;i++){
-		aBtn[i].className="";
+	function doMove(node,off){
+		var target = -off*aImg[0].offsetWidth;
+		var speed = (target-node.offsetLeft)/10;
+		var oldOffsetLeft = node.offsetLeft;
+		speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+		node.offsetLeft == target ? clearInterval(timer):node.style.left = node.offsetLeft+speed+"px";
 	}
-	aBtn[index].className="active";
-	doChange(oImgList,index);
+
+	function startMove(node,off){
+		clearInterval(timer);
+		timer = setInterval(function(){
+			doMove(node,off);
+		},30)
+	}
+
+	function doChange(node,off){
+		node.style.left = (-off*aImg[0].offsetWidth)+"px";
+	}
+
+	function next(){
+		if(index<=0)
+			isBorder=true;
+		if(index>=aImg.length-1)
+			isBorder = false;
+		isBorder ? index++ : index--;
+		show();
+	}
+
+	function show(){
+		for(i=0;i<aImg.length;i++){
+			aBtn[i].className = "";
+		}
+		aBtn[index].className = "active";
+		//doChange(oImgList,index);
+		startMove(oImgList,index);
+	}
+
+	autoTimer=setInterval(next,2000);
+
+	for(i=0;i<aBtn.length;i++){
+		aBtn[i].int = i;
+		aBtn[i].onclick = function(){
+			index=this.int;
+			show();
+		}
+	}
+
+	oImgBox.onmouseover = function(){
+		clearInterval(autoTimer);
+	}
+
+	oImgBox.onmouseout = function(){
+		autoTimer=setInterval(next,2000);
+	}
 }
 
-setInterval(next,2000);
+function addLoadEvent(func){
+	var oldOnload = window.onload;
+	if (typeof oldOnload != "function"){
+		window.onload = func;
+	} else{
+		window.onload = function(){
+			oldOnload();
+			func();
+		}
+	}
+}
 
-};
+addLoadEvent(carousel);
